90176d006f591b5f81069025f26d13c23a6bb48a
nswai983 <52177900+nswai983@users.noreply.github.com>
2020-02-25 16:23:17 -0500

    Add files via upload

---
+++ webapp/app.js
@@ -0,0 +1,38 @@
+let express = require("express");
+
+let mysql = require("mysql");
+
+let dbhost = process.env['DBHOST'] || 'localhost';
+
+function getConnection() {
+    return mysql.createConnection({
+    "host": dbhost,
+    "port": 3306,
+    "user": "mysql",
+    "password": "mysql",
+    "database": "mydb"
+    });
+}
+
+let app = express();
+
+app.get('/hello', function (req, res) {
+    let connection = getConnection();
+    connection.connect(function (err) {
+        if (err) {
+            console.log("Problem connecting to database", err);
+            res.send("Unable to connect to database! " + err);
+            return;
+        }
+        connection.query("SELECT * FROM Person", function (err, results) {
+            res.send(results);
+            connection.destroy();
+        });
+    });    
+});
+
+let port = process.env['PORT'] || 8888;
+port = parseInt(port)
+app.listen(port, function () {
+    console.log('Express server listening on port ' + port);
+});
