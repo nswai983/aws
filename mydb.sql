ade445a3044d084bc5bf3c415592e0b5963168e3
nswai983 <52177900+nswai983@users.noreply.github.com>
2020-02-25 16:32:48 -0500

    Add files via upload

--- mydb.sql
+++ mydb.sql
@@ -5,3 +5,14 @@ create table Person (
 
 insert into Person values (1, 'Fred Jones');
 insert into Person values (2, 'Alf Wilson');
+
+create table Registrations (
+  registration_id integer primary key,
+  firstName varchar(30),
+  lastName varchar(30),
+  grade integer,
+  email varchar(50),
+  shirtSize char(1),
+  hrUsername varchar(30)
+);
+
--- webapp/app.js
+++ webapp/app.js
@@ -1,5 +1,8 @@
-let express = require("express");
+'use strict';
 
+var express = require('express');
+// var exphbs  = require('express-handlebars');
+// var bodyParser = require('body-parser');
 let mysql = require("mysql");
 
 let dbhost = process.env['DBHOST'] || 'localhost';
@@ -16,7 +19,7 @@ function getConnection() {
 
 let app = express();
 
-app.get('/hello', function (req, res) {
+app.get('/registrations', function (req, res) {
     let connection = getConnection();
     connection.connect(function (err) {
         if (err) {
@@ -24,11 +27,73 @@ app.get('/hello', function (req, res) {
             res.send("Unable to connect to database! " + err);
             return;
         }
-        connection.query("SELECT * FROM Person", function (err, results) {
-            res.send(results);
+        connection.query("SELECT * FROM Registrations", function (err, results) {
+            if (err) {
+                res.send("Error! " + err)
+            }
+            res.send(JSON.parse(results));
             connection.destroy();
         });
     });    
+});
+
+app.post('/registration', function (req, res) {
+    var firstName = req.body.firstName || "";
+    var lastName = req.body.lastName || "";
+    var grade = req.body.grade || "";
+    var email = req.body.email || "";
+    var shirtSize = req.body.shirtSize || "";
+    var hrUsername = req.body.hrUsername || "";
+    var errMsg;
+
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    hrUsername = hrUsername.trim();
+
+    if (firstName == '' || lastName == '')
+    {
+        errMsg = "Please supply first and last name.";
+    }
+
+    if ((grade == '') || (grade != '9') || (grade != '10') || (grade != '11') || (grade != '12'))
+    {
+        errMsg = "Please enter grade (9, 10, 11, 12).";
+    }
+
+    if (email == '')
+    {
+        errMsg = "Please enter email.";
+    }
+
+    if ((shirtSize == '') || (shirtSize != 'S') || (shirtSize != 'M') || (shirtSize != 'L'))
+    {
+        errMsg = "Please enter your shirt size (S, M, L).";
+    }
+
+    if (hrUsername == '')
+    {
+        errMsg = "Please enter your hackerrank username.";
+    }
+
+    if (errMsg) {
+        res.status(400).send( {error: errMsg} );
+    }
+
+    let connection = getConnection();
+    connection.connect(function (err) {
+        if (err) {
+            console.log("Problem connecting to database", err);
+            res.send("Unable to connect to database! " + err);
+            return;
+        }
+        connection.query(`UPDATE Registrations VALUES (${firstName}, ${lastName}, ${grade}, ${email}, ${shirtSize}, ${hrUsername})`, function (err, results) {
+            if (err) {
+                res.send("Error! " + err)
+            }
+            connection.destroy();
+        });
+    });
+
 });
 
 let port = process.env['PORT'] || 8888;
