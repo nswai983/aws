b1caad2c74e442d626b7ac9338316a84a8f91093
nswai983 <52177900+nswai983@users.noreply.github.com>
2020-02-25 20:56:55 -0500

    Add files via upload

--- mydb.sql
+++ mydb.sql
@@ -7,7 +7,7 @@ insert into Person values (1, 'Fred Jones');
 insert into Person values (2, 'Alf Wilson');
 
 create table Registrations (
-  registration_id integer NOT NULL IDENTITY(1,1) primary key,
+  registration_id integer NOT NULL AUTO_INCREMENT primary key,
   firstName varchar(30),
   lastName varchar(30),
   grade integer,
